bbbb52744b10b230e471d2712a82793cb857ef00
drew1kun <10493450+drew1kun@users.noreply.github.com>
2026-06-17 11:07:06 -0700

    env variables, removed container name, compose file refactor

--- drew-telegraf/exports.sh
+++ drew-telegraf/exports.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
 export DOCKER_GID=$(stat -c '%g' /var/run/docker.sock)
+
+export UMBREL_HOSTNAME="$(hostname)"
+export UMBREL_SEARCH_DOMAIN=$(awk '/^search/ {print $2; exit}' /etc/resolv.conf)
